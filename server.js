2df3a9e795de4d89a6cb6f661c9eb6a999d12d06
lucapipit <pipitonelucamaria@gmail.com>
2024-01-18 16:59:37 +0100

    sistemato il multer

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors';
 const app = express();
+
 //routes
 import activeUsers from './routes/activeUsers.js';
 import announcements from './routes/announcements.js';
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 //routes
+app.use("/uploads", express.static("uploads"));
 app.use('/', activeUsers);
 app.use('/', announcements);
 app.use('/', professionals);
